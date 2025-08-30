82d6a038024c0e5d6ed34deeaa6e6b9c5af842eb
Akash Saraf <akashsaraf24@gmail.com>
2025-08-30 20:03:45 +0530

    Update and delete queries in hibernate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,11 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args){
-        Student s1 = null;
+        Student s1 = new Student();
+
+        s1.setRollNo(1);
+        s1.setsName("Akash S");
+        s1.setAge(27);
 
 
         SessionFactory sf = new Configuration()
@@ -15,7 +19,20 @@ public class Main {
 
         Session session = sf.openSession();
 
-        s1 = session.find(Student.class, 1);
+        Transaction transaction = session.beginTransaction();
+
+        // Update the student with rollno 1
+//        session.merge(s1);
+//        transaction.commit();
+
+        // Remove the student with rollno 3
+
+        Student studentToDelete = session.find(Student.class, 3);
+
+        session.remove(studentToDelete);
+        transaction.commit();
+
+
 
         session.close();
         sf.close();
